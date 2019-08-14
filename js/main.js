ec6750dca65c16a8b110195bf1cef1296739acd0
tatsunoriuehara <uehara.t29@gmail.com>
2019-08-14 12:27:09 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,54 @@
+(function(){
+  var slider=document.getElementById('slider');
+  var label=document.getElementById('label');
+  var btn=document.getElementById('btn');
+  var result=document.getElementById('result');
+  var numbers=document.getElementById('numbers');
+  var symbols=document.getElementById('symbols');
+
+
+  function getPassword(){
+    var seed_letters='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var seed_numbers='0123456789';
+    var seed_symbols='!#$%&()';
+    var seed;
+
+    var len=slider.value;
+    var pwd='';
+
+    seed=seed_letters+seed_letters.toUpperCase();
+    if(numbers.checked===true){
+      seed+=seed_numbers;
+    }
+    if(symbols.checked){
+      seed+=seed_symbols;
+    }
+    // for(var i=0;i<len;i++){
+    //   pwd+=seed[Math.floor(Math.random()*seed.length)];
+    // }
+
+    while(len--){
+      pwd+=seed[Math.floor(Math.random()*seed.length)];
+
+    }
+    result.value=pwd;
+  }
+
+  slider.addEventListener('change',function(){
+    label.innerHTML=this.value;
+  });
+
+  btn.addEventListener('click',function(){
+    getPassword();
+    //result.value='hogehoge';
+  });
+
+  result.addEventListener('click',function(){
+    this.select();
+  });
+
+  getPassword(
+
+  );
+
+})();
